951f55f295861bc2921504293d2d59d825a0f013
archisharma <archi.sharma@innovaccer.com>
2020-01-06 17:06:54 +0530

    Changed entypoint.sh for data-sync job jar creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,27 +17,16 @@ then
     exit 1
 fi
 
-if [ "${gitTag:1}" = "${releaseVersion}" ]
-then
-     if [ "${releaseVersion:0:1}" = "${platformFrameworkVersion:0:1}" ]
-     then
-         echo "versions validation done"
-     else
-         echo "major release tag component doesn't match framework version major tag component in project/Versions.scala"
-         echo "release version in build.sbt : "${releaseVersion}
-         echo "framework version in project/Versions.scala : "${platformFrameworkVersion}
-         exit 1
-     fi
-else
-    echo "git tag doesn't match release tag in build.sbt"
-    echo "tag : "${gitTag}
-    echo "release version in build.sbt : "${releaseVersion}
-    exit 1
-fi
-
 sbt clean compile assembly
 
-jar_path="$(grep "\"jar\": .*" project/Jobs | awk -F ":" '{print $2;}' | sed "s/\"//g")"
-
+jar_path="target/scala_2.10/data-sync.jar"
+mv ${jar_path} target/scala_2.10/data-sync_5.6.jar
 # Upload jar to bucket
-python -m awscli s3 cp ${jar_path} s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/
+python -m awscli s3 cp target/scala_2.10/data-sync_5.6.jar s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/
+
+sbt -DelasticVersion=2.3 clean compile assembly
+
+jar_path="target/scala_2.10/data-snc.jar"
+mv ${jar_path} target/scala_2.10/data-sync_2.3.jar
+
+python -m awscli s3 cp target/scala_2.10/data-sync_2.3.jar s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/
