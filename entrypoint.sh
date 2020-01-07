47d834e1807de0a3dd4c10d24fb9d42fa4cd1353
archisharma <archi.sharma@innovaccer.com>
2020-01-07 15:57:08 +0530

    removed profile argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ fi
 sbt clean compile assembly
 
 jar_path="/github/workspace/target/scala-2.10/data-sync.jar"
-python -m awscli s3 cp ${jar_path} s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/data-sync_5.6.jar --profile ga
+python -m awscli s3 cp ${jar_path} s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/data-sync_5.6.jar
 
 sbt -DelasticVersion=2.3 clean compile assembly
-python -m awscli s3 cp ${jar_path} s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/data-sync_2.3.jar --profile ga
+python -m awscli s3 cp ${jar_path} s3://${BUCKET_NAME}/share/lib/v${releaseVersion}/data-sync_2.3.jar
